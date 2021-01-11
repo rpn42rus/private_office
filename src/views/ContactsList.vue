33f724c73d1cd8714ac0a009453fdcea0cb28b0f
rpn42rus <68055599+rpn42rus@users.noreply.github.com>
2021-01-11 17:36:55 +0700

    Стилизация списка контактов

--- src/views/ContactsList.vue
+++ src/views/ContactsList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Список контактов</h1>
+  <div class="container">
+    <div class="title">Список контактов</div>
     <div class="grid-layout">
       <contact-card v-for="character in characters" :key="character.id" :character="character" />
     </div>
@@ -38,10 +38,20 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.grid-layout {
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-gap: 2rem;
-  gap: 2rem;
-  display: grid;
+.container {
+  max-width: 1620px;
+  margin: 0 auto;
+  padding: 0 15px;
+  .title {
+    margin: 50px 0;
+    font-size: 32px;
+    font-weight: bold;
+  }
+  .grid-layout {
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 2rem;
+    gap: 2rem;
+    display: grid;
+  }
 }
 </style>
