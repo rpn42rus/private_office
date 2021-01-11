09c37ec9daa6a31f715c11b91df8aa80a8b96642
rpn42rus <68055599+rpn42rus@users.noreply.github.com>
2021-01-11 21:40:29 +0700

    created preloader

---
+++ src/components/Preloader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="preloader-wrap" v-show="isShowPreloader">
+    <atom-spinner :animation-duration="1000" :size="100" :color="'#ff1d5e'" />
+  </div>
+</template>
+
+<script>
+import { AtomSpinner } from 'epic-spinners';
+export default {
+  name: 'Preloader',
+
+  components: {
+    AtomSpinner,
+  },
+
+  props: {
+    isShowPreloader: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.preloader-wrap {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 999;
+}
+</style>
--- src/views/ContactsList.vue
+++ src/views/ContactsList.vue
@@ -1,55 +1,59 @@
 <template>
   <div class="container">
-    <div class="title">Список контактов</div>
-    <div class="grid-layout">
-      <contact-card v-for="character in characters" :key="character.id" :character="character" />
-    </div>
-    <div class="pagination-controls">
-      <div class="prev-contacts pagination-btn" @click="goToPage(info.prev)">prev</div>
-      <div class="next-contacts pagination-btn" @click="goToPage(info.next)">next</div>
+    <div class="page-wrap">
+      <div class="page-title">Список контактов</div>
+      <div class="page-content">
+        <div class="grid-layout" v-if="!loading">
+          <contact-card
+            v-for="character in characters"
+            :key="character.id"
+            :character="character"
+          />
+        </div>
+      </div>
+      <div class="pagination-controls">
+        <div class="prev-contacts pagination-btn" @click="goToPage(info.prev)">prev</div>
+        <div class="next-contacts pagination-btn" @click="goToPage(info.next)">next</div>
+      </div>
+      <preloader :isShowPreloader="loading" />
     </div>
   </div>
 </template>
 
 <script>
-import ContactCard from './ContactCard.vue';
+import ContactCard from '../components/ContactCard';
+import Preloader from '../components/Preloader.vue';
 export default {
-  components: { ContactCard },
+  components: { ContactCard, Preloader },
   name: 'ContactsList',
 
   data() {
     return {
       info: {},
       characters: [],
-      nextPage: null,
+      loading: false,
     };
   },
 
   mounted() {
+    this.loading = true;
     this.axios.get('https://rickandmortyapi.com/api/character').then(response => {
       this.characters = response.data.results;
       this.info = response.data.info;
+      this.loading = false;
     });
   },
 
   methods: {
     goToPage(page) {
+      this.loading = true;
       this.axios.get(`${page}`).then(response => {
         this.characters = response.data.results;
         this.info = response.data.info;
+        this.loading = false;
       });
     },
   },
-
-  // created: async function() {
-  //   try {
-  //     const res = await fetch('https://rickandmortyapi.com/api/character');
-  //     const characters = await res.json();
-  //     console.log('characters :>> ', characters);
-  //   } catch (error) {
-  //     console.log('error :>> ', error);
-  //   }
-  // },
 };
 </script>
 
@@ -58,18 +62,27 @@ export default {
   max-width: 1620px;
   margin: 0 auto;
   padding: 0 15px;
-  .title {
-    margin: 50px 0;
-    font-size: 32px;
-    color: rgb(41, 41, 41);
-    font-weight: bold;
+  .page-wrap {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    .page-title {
+      padding: 50px 0;
+      font-size: 32px;
+      color: rgb(41, 41, 41);
+      font-weight: bold;
+    }
+    .page-content {
+      flex: 1 1 auto;
+      .grid-layout {
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        grid-gap: 2rem;
+        gap: 2rem;
+        display: grid;
+      }
+    }
   }
-  .grid-layout {
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    grid-gap: 2rem;
-    gap: 2rem;
-    display: grid;
-  }
+
   .pagination-controls {
     display: flex;
     justify-content: center;
