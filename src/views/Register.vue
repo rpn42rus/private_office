ddd23fd3b3c16aeacf54632a3170f44e95aa5596
rpn42rus <68055599+rpn42rus@users.noreply.github.com>
2021-01-19 12:11:56 +0700

    validate register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,33 +6,65 @@
         <label class="field__label">Name:</label>
         <input
           class="field__input"
-          required
-          v-model.trim="$v.name.$model"
+          v-model.trim="name"
           type="text"
           placeholder="Name"
+          :class="{
+            invalid:
+              ($v.name.$dirty && !$v.name.required) || ($v.name.$dirty && !$v.name.minLength),
+          }"
         />
-        <div class="error" v-if="!$v.name.required">Field is required</div>
-        <div class="error" v-if="!$v.name.minLength">
-          Name must have at least {{ $v.name.$params.minLength.min }} letters.
-        </div>
+        <small class="invalid" v-if="$v.name.$dirty && !$v.name.required">Заполните поле</small>
+        <small class="invalid" v-else-if="$v.name.$dirty && !$v.name.minLength"
+          >Имя должно состоять из {{ $v.name.$params.minLength.min }} символов
+        </small>
         <label class="field__label">Email:</label>
-        <input class="field__input" required v-model="email" type="email" placeholder="Email" />
+        <input
+          class="field__input"
+          v-model.trim="email"
+          type="email"
+          placeholder="Email"
+          :class="{
+            invalid:
+              ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email),
+          }"
+        />
+        <small class="invalid" v-if="$v.email.$dirty && !$v.email.required">Заполните поле</small>
+        <small class="invalid" v-else-if="$v.email.$dirty && !$v.email.email"
+          >Введите корректный email</small
+        >
         <label class="field__label">Password:</label>
         <input
           class="field__input"
-          required
-          v-model="password"
+          v-model.trim="password"
           type="password"
           placeholder="Password"
+          :class="{
+            invalid:
+              ($v.password.$dirty && !$v.password.required) ||
+              ($v.password.$dirty && !$v.password.minLength),
+          }"
         />
+        <small class="invalid" v-if="$v.password.$dirty && !$v.password.required"
+          >Заполните поле</small
+        >
+        <small class="invalid" v-else-if="$v.password.$dirty && !$v.password.minLength"
+          >Пароль должен быть не менее {{ $v.password.$params.minLength.min }} символов, сейчас
+          {{ password.length }} символов</small
+        >
       </div>
+      <div class="reg__checkbox">
+        <input type="checkbox" id="checkbox" v-model="agree" />
+        <label for="checkbox">С правилами согласен</label>
+      </div>
+
       <button class="reg__btn" type="submit">Зарегистрироваться</button>
     </form>
   </div>
 </template>
 
 <script>
-import { required, minLength } from 'vuelidate/lib/validators';
+import { email, required, minLength } from 'vuelidate/lib/validators';
 
 export default {
   data() {
@@ -40,13 +72,44 @@ export default {
       name: '',
       email: '',
       password: '',
+      agree: false,
     };
   },
 
   validations: {
     name: {
       required,
+      minLength: minLength(3),
+    },
+    email: {
+      email,
+      required,
+    },
+    password: {
+      required,
       minLength: minLength(4),
+    },
+    agree: {
+      cheched: value => value,
+    },
+  },
+
+  methods: {
+    reg() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+      const formData = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      };
+
+      console.log('formData :>> ', formData);
+
+      this.$router.push('/');
     },
   },
 };
@@ -79,6 +142,14 @@ export default {
       outline: none;
     }
   }
+  &__checkbox {
+    display: flex;
+    margin: 10px 0px;
+    input {
+      margin-right: 10px;
+      outline: none;
+    }
+  }
   &__btn {
     width: 50%;
     margin: 20px auto;
@@ -97,5 +168,9 @@ export default {
       cursor: pointer;
     }
   }
+}
+
+.invalid {
+  color: red;
 }
 </style>
